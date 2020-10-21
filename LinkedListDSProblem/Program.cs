470643663a8f2bfe2c458da50bef5e481010a16d
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-21 18:21:50 +0530

    [Mihir] Add Welcome message

---
+++ LinkedListDSProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListDSProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Data Structure Problem.");
+        }
+    }
+}
